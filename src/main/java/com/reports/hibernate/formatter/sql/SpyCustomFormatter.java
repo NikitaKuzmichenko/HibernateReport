e654bf14690fcc44fc77f8a2bea09970edbcbae4
nikitak <n.kuzmichenko@isida.by>
2023-09-22 11:26:15 +0300

    added logging, using p6spy

---
+++ src/main/java/com/reports/hibernate/formatter/sql/SpyCustomFormatter.java
@@ -0,0 +1,24 @@
+package com.reports.hibernate.formatter.sql;
+
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
+import org.hibernate.engine.jdbc.internal.Formatter;
+
+public class SpyCustomFormatter implements MessageFormattingStrategy {
+    private static final Formatter HIBERNATE_SQL_FORMATTER = new BasicFormatterImpl();
+    private static final String FORMAT = """
+            Category: %s\s
+            ElapsedTime: %s ms\s
+            Hibernate: %s\s
+            ==============================================================""";
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared,
+            String sql, String url) {
+        if (sql.isEmpty()) {
+            return "";
+        }
+        return String.format(FORMAT, category, elapsed, HIBERNATE_SQL_FORMATTER.format(sql));
+    }
+
+}
