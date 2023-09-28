e013981bb42969a341682fd5c8a23791a0e2c697
nikitak <n.kuzmichenko@isida.by>
2023-09-28 14:23:28 +0300

    possibility to leave msg on assertion fail, using AssertQueryCount

--- src/main/java/com/reports/hibernate/sql/query/assertion/AssertQueryCount.java
+++ src/main/java/com/reports/hibernate/sql/query/assertion/AssertQueryCount.java
@@ -23,34 +23,68 @@ public class AssertQueryCount {
         assertCount(expected, counterInfo.getAllCount(), " ");
     }
 
+    public static void assertAllCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getAllCount(), " ", msg);
+    }
+
     public static void assertSelectCount(int expected) {
         assertCount(expected, counterInfo.getSelectCount(), "SELECT");
+    }
+
+    public static void assertSelectCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getSelectCount(), "SELECT", msg);
     }
 
     public static void assertNextValCount(int expected) {
         assertCount(expected, counterInfo.getNextvalCount(), "NEXT VAL");
     }
 
+    public static void assertNextValCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getNextvalCount(), "NEXT VAL", msg);
+    }
+
     public static void assertUpdateCount(int expected) {
         assertCount(expected, counterInfo.getUpdateCount(), "UPDATE");
+    }
+
+    public static void assertUpdateCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getUpdateCount(), "UPDATE", msg);
     }
 
     public static void assertInsertCount(int expected) {
         assertCount(expected, counterInfo.getInsertCount(), "INSERT");
     }
 
+    public static void assertInsertCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getInsertCount(), "INSERT", msg);
+    }
+
     public static void assertDeleteCount(int expected) {
         assertCount(expected, counterInfo.getDeleteCount(), "DELETE");
+    }
+
+    public static void assertDeleteCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getDeleteCount(), "DELETE", msg);
     }
 
     public static void assertCallCount(int expected) {
         assertCount(expected, counterInfo.getCallCount(), "CALL");
     }
 
+    public static void assertCallCount(int expected, String msg) {
+        assertCount(expected, counterInfo.getCallCount(), "CALL", msg);
+    }
 
     private static void assertCount(int expected, int actual, String queryType) {
         if (expected != actual) {
             throw new QueryCountMismatchException(expected, actual, queryType);
         }
     }
+
+    private static void assertCount(int expected, int actual, String queryType, String msg) {
+        if (expected != actual) {
+            throw new QueryCountMismatchException(expected, actual, queryType, msg);
+        }
+    }
+
 }
--- src/main/java/com/reports/hibernate/sql/query/assertion/exception/QueryCountMismatchException.java
+++ src/main/java/com/reports/hibernate/sql/query/assertion/exception/QueryCountMismatchException.java
@@ -6,4 +6,9 @@ public class QueryCountMismatchException extends RuntimeException {
         super("Expected " + queryType + " queries amount: " + expected + " , actual: " + actual);
         this.setStackTrace(new StackTraceElement[0]); // clear stackTrace
     }
+
+    public QueryCountMismatchException(int expected, int actual, String queryType, String message){
+        super(message + "\n" + "Expected " + queryType + " queries amount: " + expected + " , actual: " + actual);
+        this.setStackTrace(new StackTraceElement[0]); // clear stackTrace
+    }
 }
