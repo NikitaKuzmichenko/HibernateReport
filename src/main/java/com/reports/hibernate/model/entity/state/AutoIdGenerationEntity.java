7d0af8894d88f7dbddf1dcc34c3dbc1ed500814d
nikit <nikita.kuzmichenko@gmail.com>
2023-10-28 12:31:38 +0300

    Entities renamed

--- src/main/java/com/reports/hibernate/model/entity/state/AutoIdGenerationEntity.java
+++ src/main/java/com/reports/hibernate/model/entity/state/AutoIdGenerationEntity.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 @Data
 @Entity
-public class AutoIdGenerationUser {
+public class AutoIdGenerationEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/reports/hibernate/model/entity/state/ManualIdGenerationEntity.java
+++ src/main/java/com/reports/hibernate/model/entity/state/ManualIdGenerationEntity.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @Entity
-public class ManualIdGenerationUser {
+public class ManualIdGenerationEntity {
 
     @Id
     private Long id;
